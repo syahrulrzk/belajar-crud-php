cfea00cece73705a3b2b930b58f9e04a9931060b
syahrulrzk <syahrulrizki318@gmail.com>
2023-10-10 11:12:17 +0700

    update code prose.php

--- proses.php
+++ proses.php
@@ -2,6 +2,16 @@
 	if(isset ($_POST['aksi'])) {
 		if($_POST['aksi'] == "add"){
 			echo "Tambahakan Data Succsess <a href='index.php'>[ Home ] </a>";
+
+			$nisn	= $_POST['nisn'];
+			$nama_siswa	= $_POST['nama_siswa'];
+			$jenis_kelamin	= $_POST['jenis_kelamin'];
+			$foto	= "img/img-anime/img5.jpg";
+			$alamat = $_POST['alamat'];
+
+
+
+
 		} else if ($_POST['aksi'] == "edit"){
 			echo "Edit Data Succsess <a href='index.php'>[ Home ]</a>";
 				
