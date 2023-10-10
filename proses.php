13110412e1321afa51e46da51ed23edc664b809e
syahrulrzk <syahrulrizki318@gmail.com>
2023-10-10 11:14:45 +0700

    update code

--- proses.php
+++ proses.php
@@ -1,13 +1,15 @@
 <?php 
 	if(isset ($_POST['aksi'])) {
 		if($_POST['aksi'] == "add"){
-			echo "Tambahakan Data Succsess <a href='index.php'>[ Home ] </a>";
+			// echo "Tambahakan Data Succsess <a href='index.php'>[ Home ] </a>";
 
 			$nisn	= $_POST['nisn'];
 			$nama_siswa	= $_POST['nama_siswa'];
 			$jenis_kelamin	= $_POST['jenis_kelamin'];
 			$foto	= "img/img-anime/img5.jpg";
 			$alamat = $_POST['alamat'];
+
+			echo $nisn." | ".$nama_siswa." | ".$jenis_kelamin." | ".$foto." | ".$alamat;
 
 
 
